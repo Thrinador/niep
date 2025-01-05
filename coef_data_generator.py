6af031653ee4adb3825f9da17d4b46ec7c735ff3
Ben <thirinador1@gmail.com>
2025-01-05 09:26:10 -0800

    Swtich to pathos for multiprocessing. This allow the removal of the ugly wrapper functions and will allow for greater flexibility for which functions to optimize over.

--- coef_data_generator.py
+++ coef_data_generator.py
@@ -1,29 +1,13 @@
 import tomli
 import time
 import json
-from multiprocessing import Pool
+from pathos.pools import ProcessPool as Pool
 from coef_optimizer import *
 
 with open("config.toml", "rb") as f:
     data = tomli.load(f)
 
 points_dim = data['global_data']['points_dim']
-
-def compute_min_y(x):
-    return optimize_func(1, [[0, x]])
-
-def compute_max_y(x):
-    results = optimize_func(1 + n, [[0 + n, -x]])
-    results.fun *= -1
-    return results
-
-def compute_min_z(point):
-    return optimize_func(2, [[0,point[0]], [1,point[1]]])
-
-def compute_max_z(point):
-    results = optimize_func(2+n, [[0+n,-point[0]], [1+n,-point[1]]])
-    results.fun *= -1
-    return results
 
 def convert_optimize_result_to_dict(result):
     """Converts a scipy OptimizeResult object to a dictionary."""
@@ -77,8 +61,10 @@ if __name__ == '__main__':
     start_time = time.perf_counter()
     x_values = np.linspace(0, n, points_dim[0])
     with Pool() as pool:
-        min_y_values = pool.map(compute_min_y, x_values)
-        max_y_values = pool.map(compute_max_y, x_values)
+        min_y_values = pool.map(lambda x: optimize_func(1, [[0, x]]), x_values)
+        max_y_values = pool.map(lambda x: optimize_func(1 + n, [[0 + n, -x]]), x_values)
+        for max_y_val in max_y_values:
+            max_y_val.fun *= -1
 
     X, Y = build_XY_mesh(x_values, min_y_values, max_y_values)
 
@@ -86,9 +72,11 @@ if __name__ == '__main__':
     start_time = time.perf_counter()
 
     with Pool() as pool:
-        Z_min = pool.map(compute_min_z, zip(X,Y))
+        Z_min = pool.map(lambda point: optimize_func(2, [[0,point[0]], [1,point[1]]]), zip(X,Y))
         print(f"Z_min done in: {time.perf_counter() - start_time:.6f} seconds")
-        Z_max = pool.map(compute_max_z, zip(X,Y))
+        Z_max = pool.map(lambda point: optimize_func(2+n, [[0+n,-point[0]], [1+n,-point[1]]]), zip(X,Y))
+        for max_z_val in Z_max:
+            max_z_val.fun *= -1
 
     print(f"Time taken to get coef data: {time.perf_counter() - start_time:.6f} seconds")
          
