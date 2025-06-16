5d80a30dfb7fa9b92e2f10dd24279a34630f8501
Ben <thrinador1@gmail.com>
2025-06-16 15:15:34 -0700

    Update the plotting to do every slice. There is a slight bug here, right now duplicate points in the lower dimensions are placed. Should add a filter to bring plot sizes down.

--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -41,11 +41,6 @@ def run_plotting(config):
         all_eigs.append(entry.get("eigenvalues") if isinstance(entry.get("eigenvalues"), list) else None)
     logging.info("Finished extracting data.")
 
-    # 3. Create the plots
-    coef_plot = create_coeff_plot(n, all_coeffs, plot_dir)
-    eig_plot = create_eig_plot(n, all_eigs, plot_dir)
-
-    # 4. Save the plots
     try: 
         os.makedirs(plot_dir, exist_ok=True)
         logging.info(f"Ensured plot directory exists: {plot_dir}")
@@ -53,64 +48,101 @@ def run_plotting(config):
         logging.error(f"Could not create plot dir: {e}")
         exit()
 
-    coef_plot.write_html(os.path.join(plot_dir, "coeffs.html"), include_mathjax='cdn')
-    eig_plot.write_html(os.path.join(plot_dir, "eigs.html"), include_mathjax='cdn')
+    create_plots(n, plot_dir, "coefficients", all_coeffs)
+    create_plots(n, plot_dir, "eigenvalues", all_eigs)
 
     print("Plot generation finished.")
 
-def create_coeff_plot(n, all_coeffs, run_identifier):
-    logging.info("Generating coefficient plot...")
+def create_plots_helper_2d(run_identifier, plot_type, all_points, i, j):
+    logging.info(f"Generating {plot_type} 2d plot for indicies {i+1}, {j+1}")
+    coeffs_x, coeffs_y = [], []
+    for coeffs in all_points:
+        coeffs_x.append(coeffs[i])
+        coeffs_y.append(coeffs[j])
+
     fig_coeffs = go.Figure()
     marker_config = dict(size=5)
 
-    if n == 3:
-        coeffs_x, coeffs_y = [], []
-        for coeffs in all_coeffs:
-            coeffs_x.append(coeffs[0])
-            coeffs_y.append(coeffs[1])
+    fig_coeffs.add_trace(go.Scatter(x=coeffs_x, y=coeffs_y, mode='markers', marker=marker_config))
+    fig_coeffs.update_layout(title=f'{plot_type} space ({run_identifier}) for E_{i+1} and E_{j+1}', 
+                            scene=dict(
+                                xaxis_title=f"{plot_type} {i+1}", 
+                                yaxis_title=f"{plot_type} {j+1}", 
+                            ))
 
-        fig_coeffs.add_trace(go.Scatter(x=coeffs_x, y=coeffs_y, mode='markers', marker=marker_config))
-        fig_coeffs.update_layout(title=f'Coefficient Space ({run_identifier})', 
-                                scene=dict(
-                                    xaxis_title=f"Coefficient 1", 
-                                    yaxis_title="Coefficient 2", 
-                                ), 
-                                margin=dict(l=0, r=0, b=0, t=40))
+    fig_coeffs.write_html(os.path.join(run_identifier, f"{plot_type}_2d_{i+1}_{j+1}.html"), include_mathjax='cdn')
 
-    elif n == 4:
-        coeffs_x, coeffs_y, coeffs_z = [], [], []
-        for coeffs in all_coeffs:
-            coeffs_x.append(coeffs[0])
-            coeffs_y.append(coeffs[1])
-            coeffs_z.append(coeffs[2])
+def create_plots_helper_3d(run_identifier, plot_type, all_points, i, j, k):
+    logging.info(f"Generating {plot_type} 3d plot for indicies {i+1}, {j+1}, {k+1}")
+    coeffs_x, coeffs_y, coeffs_z = [], [], []
+    for coeffs in all_points:
+        coeffs_x.append(coeffs[i])
+        coeffs_y.append(coeffs[j])
+        coeffs_z.append(coeffs[k])
 
-        fig_coeffs.add_trace(go.Scatter3d(x=coeffs_x, y=coeffs_y, z=coeffs_z, mode='markers', marker=marker_config))
-        fig_coeffs.update_layout(title=f'Coefficient Space ({run_identifier})', 
-                                scene=dict(
-                                    xaxis_title=f"Coefficient 1", 
-                                    yaxis_title="Coefficient 2", 
-                                    zaxis_title="Coefficient 3"
-                                ), 
-                                margin=dict(l=0, r=0, b=0, t=40))
-    elif n == 5:
-        coeffs_x, coeffs_y, coeffs_z, coeffs_color = [], [], [], []
-        for coeffs in all_coeffs:
-            coeffs_x.append(coeffs[0])
-            coeffs_y.append(coeffs[1])
-            coeffs_z.append(coeffs[2])
-            coeffs_color.append(coeffs[3])
+    fig_coeffs = go.Figure()
+    marker_config = dict(size=5)
 
-        marker_config.update(dict(color=coeffs_color, colorscale='Viridis', showscale=True, colorbar_title="Coefficient 4"))
-        fig_coeffs.add_trace(go.Scatter3d(x=coeffs_x, y=coeffs_y, z=coeffs_z, mode='markers', marker=marker_config))
-        fig_coeffs.update_layout(title=f'Coefficient Space ({run_identifier})', 
-                                scene=dict(
-                                    xaxis_title=f"Coefficient 1", 
-                                    yaxis_title="Coefficient 2", 
-                                    zaxis_title="Coefficient 3"
-                                ), 
-                                margin=dict(l=0, r=0, b=0, t=40))
+    fig_coeffs.add_trace(go.Scatter3d(x=coeffs_x, y=coeffs_y, z=coeffs_z, mode='markers', marker=marker_config))
+    fig_coeffs.update_layout(title=f'{plot_type} Space ({run_identifier}) for E_{i+1}, E_{j+1}, and E_{k+1}', 
+                            scene=dict(
+                                xaxis_title=f"{plot_type} {i+1}", 
+                                yaxis_title=f"{plot_type} {j+1}", 
+                                zaxis_title=f"{plot_type} {k+1}"
+                            ), 
+                            margin=dict(l=0, r=0, b=0, t=40))
 
-    return fig_coeffs
+    fig_coeffs.write_html(os.path.join(run_identifier, f"{plot_type}_3d_{i+1}_{j+j}_{k+1}.html"), include_mathjax='cdn')
+
+def create_plots_helper_4d(run_identifier, plot_type, all_points, i, j, k, l):
+    logging.info(f"Generating {plot_type} 4d plot for indicies {i+1}, {j+1}, {k+1}, {l+1}")
+    coeffs_x, coeffs_y, coeffs_z, coeffs_color = [], [], [], []
+    for coeffs in all_points:
+        coeffs_x.append(coeffs[i])
+        coeffs_y.append(coeffs[j])
+        coeffs_z.append(coeffs[k])
+        coeffs_color.append(coeffs[l])
+
+    fig_coeffs = go.Figure()
+    marker_config = dict(size=5)
+
+    marker_config.update(dict(color=coeffs_color, colorscale='Viridis', showscale=True, colorbar_title=f"{plot_type} {l+1}"))
+    fig_coeffs.add_trace(go.Scatter3d(x=coeffs_x, y=coeffs_y, z=coeffs_z, mode='markers', marker=marker_config))
+    fig_coeffs.update_layout(title=f'{plot_type} Space ({run_identifier}) for E_{i+1}, E_{j+1}, E_{k+1}, and E_{l+1}', 
+                            scene=dict(
+                                xaxis_title=f"{plot_type} {i+1}", 
+                                yaxis_title=f"{plot_type} {j+1}", 
+                                zaxis_title=f"{plot_type} {k+1}"
+                            ), 
+                            margin=dict(l=0, r=0, b=0, t=40))
+
+
+def create_plots(n, run_identifier, plot_type, all_points):
+    logging.info(f"Generating {plot_type} plot...")
+    fig_coeffs = go.Figure()
+    marker_config = dict(size=5)
+
+    # Make all the 2d plots
+    if n >= 3:
+        for i in range(len(all_points[0])):
+            for j in range(i+1, len(all_points[0])):
+                create_plots_helper_2d(run_identifier, plot_type, all_points, i,j)
+
+    # Make all the 3d plots
+    if n >= 4:
+        for i in range(len(all_points[0])):
+            for j in range(i+1, len(all_points[0])):
+                for k in range(j+1, len(all_points[0])):
+                    create_plots_helper_3d(run_identifier, plot_type, all_points, i,j,k)
+
+    # Make all the 4d plots
+    if n >= 5:
+        for i in range(len(all_points[0])):
+            for j in range(i+1, len(all_points[0])):
+                for k in range(j+1, len(all_points[0])):
+                    for l in range(k+1, len(all_points[0])):
+                        create_plots_helper_4d(run_identifier, plot_type, all_points, i,j,k,l)
+
 
 def create_eig_plot(n, all_eigs, run_identifier):
     logging.info("Generating overall eigenvalue plot...")
